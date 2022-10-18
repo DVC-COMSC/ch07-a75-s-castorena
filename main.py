93ee9ae264aa229ce7b099f13f8c9cfe1b3b2c59
sarah castorena <ceanz@yahoo.com>
2022-10-17 23:19:21 -0700

    new

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 
 import sys
 
-num1 = list(map(int, input().split()))
-num2 = list(map(int, input().split()))
+num1 = list(map(int, input("Enter the subset: ").split()))
+num2 = list(map(int, input("Enter the primary set: ").split()))
 
 if len(num1) > len(num2):
 	print ('False')
@@ -10,5 +10,13 @@ if len(num1) > len(num2):
 # ******************************
 # Make your Code
 # ******************************
-
+count = 0
+for i in range(len(num2) - len(num1) + 1): 
+   for j in range(len(num1)):
+      if num1[j] == num2[i+j]:
+         count = count + 1
+if count == 3:
+	print('True')
+else:
+	print('False')		
 # print ('True') or ('False')
